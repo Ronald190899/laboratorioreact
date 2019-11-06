b2b39255d5f0e68acbd70b82fbd849d359266c2e
Ronald190899 <00104518@uca.edu.sv>
2019-11-05 19:28:05 -0600

    Updates

---
+++ precache-manifest.b09645e9eac8c5e537cd5acd69dde6e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e82e325cf5921fe2eccfa495f604d47",
+    "url": "/laboratorioreact/index.html"
+  },
+  {
+    "revision": "750de90afc953d403488",
+    "url": "/laboratorioreact/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dbaad4deffe1245c4bf1",
+    "url": "/laboratorioreact/static/js/2.65fe9fd1.chunk.js"
+  },
+  {
+    "revision": "750de90afc953d403488",
+    "url": "/laboratorioreact/static/js/main.ee3a3813.chunk.js"
+  },
+  {
+    "revision": "c882daa68100e2653b25",
+    "url": "/laboratorioreact/static/js/runtime-main.d149f2d4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9760eb85eef5e3e485498a6bdb6bab6.js"
+  "/laboratorioreact/precache-manifest.b09645e9eac8c5e537cd5acd69dde6e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laboratorioreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
