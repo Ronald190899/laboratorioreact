9b50b6eedfa7363049ee23c83c9a5fc752865d96
Ronald190899 <00104518@uca.edu.sv>
2019-11-05 19:16:07 -0600

    Updates

---
+++ precache-manifest.b9760eb85eef5e3e485498a6bdb6bab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7795aba2aa3dc48419f5913fbb59c6e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750de90afc953d403488",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dbaad4deffe1245c4bf1",
+    "url": "/static/js/2.65fe9fd1.chunk.js"
+  },
+  {
+    "revision": "750de90afc953d403488",
+    "url": "/static/js/main.ee3a3813.chunk.js"
+  },
+  {
+    "revision": "281033f8d5e3b1487a2a",
+    "url": "/static/js/runtime-main.d1513aee.js"
+  }
+]);
